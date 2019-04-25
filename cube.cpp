f93c0b56503222415569f7b0439738609da584cc
Adith kumar <adithkumar.ba@gmail.com>
2019-04-25 19:32:00 +0530

    Update cube.cpp

--- cube.cpp
+++ cube.cpp
@@ -11,8 +11,7 @@ void init()
 	glMatrixMode(GL_MODELVIEW);
 }
 float co[7][3] = { {0.3,0.8,0 }, { 0,0.5,1 }, { 1,0.8,0 }, { 0.9,0.9,0.9 }, { 1,0.4,0 }, { 0.9,0,0 }, { 0.2,0.2,0.2 } };
-//float v[8][3] = { {-1, 1, -1},{1, 1, -1},{1, -1, -1},{-1, -1, -1} ,{-1, 1, 1},{1, 1, 1},{1, -1, 1},{-1, -1, 1} };
-float size = 1.0;
+float v[8][8][3];
 // ----------------------------------------------------------
 // Function Prototypes
 // ----------------------------------------------------------
@@ -75,6 +74,43 @@ void drawcube(float *a1,float *a2,float *a3 , float *a4 , float *a5,float *a6 , 
 	
 }
 
+void makepoints()
+{
+	double start = 1.0;
+	double small_size = 0.975;
+	double intercube_spacing = 0.05;
+	int i = 0;
+
+	for (double z = start; z > -start; z -= small_size + intercube_spacing) {
+		for (double y = start; y > -start; y -= small_size + intercube_spacing) {
+			for (double x = -start; x < start; x += small_size + intercube_spacing) {
+
+				float a[8][3] = {
+					{ x , y, z },
+					{ x + small_size, y, z },
+					{ x + small_size, y - small_size, z },
+					{ x , y - small_size, z },
+					{ x , y, z - small_size },
+					{ x + small_size, y, z - small_size },
+					{ x + small_size, y - small_size, z - small_size },
+					{ x , y - small_size, z - small_size }
+				};
+				for (int j = 0; j < 8; j++)
+					for (int k = 0; k < 3; k++)
+						v[i][j][k] = a[j][k];
+				i += 1;
+			}
+		}
+	}
+}
+
+
+void buildcube()
+{
+	for(int i=0;i<8;i++)
+		drawcube(v[i][0], v[i][1], v[i][2], v[i][3], v[i][4], v[i][5], v[i][6], v[i][7]);			
+}
+
 void display();
 void specialKeys();
 
@@ -88,50 +124,18 @@ double rotate_x = 0;
 // display() Callback function
 // ----------------------------------------------------------
 void display() {
-
 	//  Clear screen and Z-buffer
 	glClearColor(0, 0, 0, 1);
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-	
 	glLoadIdentity();
-
-	glPushMatrix();
 	
 	glRotatef(rotate_x, 1.0, 0.0, 0.0);
 	glRotatef(rotate_y, 0.0, 1.0, 0.0);
+	makepoints();
+	buildcube();
 	
-	double start = 1.0;
-	double small_size = 0.975;
-	double intercube_spacing = 0.05;
-	
-	for (double z = start; z > -start; z -= small_size + intercube_spacing) {
-		for (double y = start; y > -start; y -= small_size + intercube_spacing) {
-			for (double x = -start; x < start; x += small_size + intercube_spacing) {
-
-				float v[8][3] = {
-					{ x , y, z },
-					{ x + small_size, y, z },
-					{ x + small_size, y - small_size, z },
-					{ x , y - small_size, z },
-					{ x , y, z - small_size },
-					{ x + small_size, y, z - small_size },
-					{ x + small_size, y - small_size, z - small_size },
-					{ x , y - small_size, z - small_size }
-				};
-				//printf_s("%lf \n", v[0][2]);
-				drawcube(v[0], v[1], v[2], v[3], v[4], v[5], v[6], v[7]);
-				
-			}
-		}
-	}
-	// Other Transformations
-	// glScalef( 2.0, 2.0, 0.0 );          // Not included
-	//drawcube(v[0],v[1],v[2],v[3],v[4],v[5],v[6],v[7]);
-	//Multi-colored side - FRONT
-	
-	glPopMatrix();
-	glFlush();
+	glutSwapBuffers();
 
 }
 
@@ -143,21 +147,26 @@ void specialKeys(int key, int x, int y) {
 	//  Right arrow - increase rotation by 5 degree
 	if (key == GLUT_KEY_RIGHT)
 	{
-		glRotatef(5, 0.0, 1.0, 0.0);
-		glFlush();
-	}
+		rotate_y += 5;
+	} 
 	//  Left arrow - decrease rotation by 5 degree
 	else if (key == GLUT_KEY_LEFT)
+	{
 		rotate_y -= 5;
+	}
 
 	else if (key == GLUT_KEY_UP)
+	{
 		rotate_x += 5;
+	}
 
 	else if (key == GLUT_KEY_DOWN)
+	{
 		rotate_x -= 5;
+	}
 
 	//  Request display update
-	//glutPostRedisplay();
+	glutPostRedisplay();
 
 }
 
@@ -166,26 +175,20 @@ void specialKeys(int key, int x, int y) {
 // ----------------------------------------------------------
 int main(int argc, char* argv[]) {
 
-	//  Initialize GLUT and process user parameters
 	glutInit(&argc, argv);
-
-	//  Request double buffered true color window with Z-buffer
-	glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB | GLUT_DEPTH);
+	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);
 	glutInitWindowSize(500, 500);
-	// Create window
+	
 	glutCreateWindow("dumb box");
 
-	//  Enable Z-buffer depth test
 	glEnable(GL_DEPTH_TEST);
 	init();
-	// Callback functions
+
 	glutDisplayFunc(display);
 	glutSpecialFunc(specialKeys);
-
-	//  Pass control to GLUT for events
 	glutMainLoop();
 
-	//  Return to OS
+
 	return 0;
 
 }
